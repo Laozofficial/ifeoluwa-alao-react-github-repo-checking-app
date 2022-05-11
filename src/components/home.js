9e0d52141494b458d2658cc46013d2fc54223d55
Laozofficial <laozofficial@gmail>
2022-05-11 13:02:53 +0100

    added assets to the app

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,12 @@
-
+import NavBar from './nav';
 
 const Home = () => {
     return (
-        <div class="container">
-            <h1>Hello world</h1>
+        <div>
+            <NavBar />
+            <div class="container">
+                <h1>Welcome to the home page</h1>
+            </div>
         </div>
     )
 }
