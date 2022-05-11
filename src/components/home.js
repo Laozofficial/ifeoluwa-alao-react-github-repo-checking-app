acddbe8d726b64b12069ef6c45592db067d15291
Laozofficial <laozofficial@gmail>
2022-05-11 12:55:24 +0100

    added react router to the app

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div class="container">
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/home';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="home" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
